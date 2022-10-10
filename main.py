5b05e56ef250cf4fbc1a701167f0b6b394f1cf7c
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-10 14:50:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ num2 = list(map(int, input().split()))
 # ******************************
 
 # print ('True') or print ('False')
+count = 0
+for n in num2:
+    if n in num1:
+        count += 1
+
+if count == len(num2):
+    print(True)
+else:
+    print(False)
